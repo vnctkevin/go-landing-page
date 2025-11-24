740994b474a7baf9364a31e96141e1a7f8c5af18
vnctkevin <vnctkevin@gmail.com>
2025-11-24 21:41:54 +0700

    fix: use deployment db and add umami analytics

--- config/db.go
+++ config/db.go
@@ -14,7 +14,7 @@ func ConnectDatabase() {
 	// Update these details with your local Postgres credentials
 	dsn := os.Getenv("DATABASE_URL")
 	if dsn == "" {
-		dsn = "host=localhost user=postgres password=kevin dbname=go_website port=5432 sslmode=disable" // Default dsn if not set in environment
+		dsn = "host=fccckgoc80wwkkgg48o4kg88 user=postgres password=Kev%21nKevin8320 dbname=go_site port=5432 sslmode=disable"
 	}
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
