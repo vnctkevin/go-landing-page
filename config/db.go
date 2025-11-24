10d8584bab50425843e55e749fb7bfa4aa25670f
vnctkevin <vnctkevin@gmail.com>
2025-11-24 21:44:32 +0700

    fix: database connection

--- config/db.go
+++ config/db.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"log"
-	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -12,10 +11,7 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 	// Update these details with your local Postgres credentials
-	dsn := os.Getenv("DATABASE_URL")
-	if dsn == "" {
-		dsn = "host=fccckgoc80wwkkgg48o4kg88 user=postgres password=Kev%21nKevin8320 dbname=go_site port=5432 sslmode=disable"
-	}
+	dsn := "host=fccckgoc80wwkkgg48o4kg88 user=postgres password=Kev%21nKevin8320 dbname=go_site port=5432 sslmode=disable"
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
